c8a2e7a2b312d12f9fcdf799909b4a0f61403ead
FrankCCCCC <unaxultraspaceos5@gmail.com>
2020-01-26 17:48:44 +0800

    Postlist Done

--- tea_app/src/component/gallery/Gallery.js
+++ tea_app/src/component/gallery/Gallery.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Link, Switch, Route, useParams, useRouteMatch} from 'react-router-dom';
 import {font_style} from '../theme/font';
+import {} from '../util/config'
 
 // const galleryInput = [{
 //     id: Integer,
@@ -11,51 +12,149 @@ import {font_style} from '../theme/font';
 
 // const route = String next route path
 
-function Gallery(props){
-    if(!Array.isArray(props.galleryInput)){
-        console.log("Error GalleryInput is not array")
-        throw "Error GalleryInput is not array";
-        return -1;
+class Gallery extends React.Component{
+    constructor(props){
+        super(props);
+        if(!Array.isArray(this.props.galleryInput)){
+            console.log("Error GalleryInput is not array")
+            throw "Error GalleryInput is not array";
+            return -1;
+        }
+        this.state = {
+            content: []
+        }
     }
-    var content = [];
 
-    let {path, url} = useRouteMatch();
-    console.log(`${url}`)
-    for(let i=0; i < props.galleryInput.length; i++){
-        console.log(`${url}${props.route}/${props.galleryInput[i].id}`)
-        content.push(
-            <div class="mb-3" data-aos={i % 2? "fade-right" : "fade-left"}>
-                
-                    <div style={{position: "relative", textAlign: "right", color: "white"}}>
-                        <Link to={`${url}${props.route}/${props.galleryInput[i].id}`}>
-                            <img class="img-fluid" src={props.galleryInput[i].img} alt="Card image cap" style={{width: "100%"}}/>
-                        </Link>
-                        <div style={{position: "absolute", bottom: "0rem", right: "0rem", width: "100%", paddingRight: "0.5rem", paddingLeft: "0.5rem", paddingTop: "0.3rem", backgroundColor: "rgba(0, 0, 0, 0.2)"}}>
-                            <h4>{props.galleryInput[i].caption_title}</h4>
-                            <p>{props.galleryInput[i].caption_subtitle}</p>
+    componentDidMount(){
+        // let {path, url} = useRouteMatch();
+        let url = "http://localhost:3000/post"
+        console.log(`${url}`)
+        var t = [];
+        for(let i=0; i < this.props.galleryInput.length; i++){
+            // console.log(`${url}${this.props.route}/${this.props.galleryInput[i].id}`)
+            t.push(
+                <div key={this.props.galleryInput[i].id} class="mb-3" data-aos={i % 2? "fade-right" : "fade-left"}>
+                    
+                        <div style={{position: "relative", textAlign: "right", color: "white"}}>
+                            <Link to={`${url}${this.props.route}/${this.props.galleryInput[i].id}`}>
+                                <img class="img-fluid" src={this.props.galleryInput[i].img} alt="Card image cap" style={{width: "100%"}}/>
+                            </Link>
+                            <div style={{position: "absolute", bottom: "0rem", right: "0rem", width: "100%", paddingRight: "0.5rem", paddingLeft: "0.5rem", paddingTop: "0.3rem", backgroundColor: "rgba(0, 0, 0, 0.2)"}}>
+                                <h4>{this.props.galleryInput[i].caption_title}</h4>
+                                <p>{this.props.galleryInput[i].caption_subtitle}</p>
+                            </div>
                         </div>
-                    </div>
+                    
+                </div>
+            );
+        }
+        this.setState({
+            content: t
+        })
+        console.log(this.state.content)
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot){
+        if(this.state.content.length != this.props.galleryInput.length){
+            // let {path, url} = useRouteMatch();
+            let url = "http://localhost:3000/post"
+            console.log(`${url}`)
+            var t = [];
+            for(let i=0; i < this.props.galleryInput.length; i++){
+                console.log(`${url}${this.props.route}/${this.props.galleryInput[i].id}`)
                 
+                t.push(
+                    <div key={this.props.galleryInput[i].id} class="mb-3">
+                        
+                            <div style={{position: "relative", textAlign: "right", color: "white"}}>
+                                <Link to={`${this.props.route}/${this.props.galleryInput[i].id}`}>
+                                    <img class="img-fluid" src={this.props.galleryInput[i].img} alt="Card image cap" style={{width: "100%"}}/>
+                                </Link>
+                                <div style={{position: "absolute", bottom: "0rem", right: "0rem", width: "100%", paddingRight: "0.5rem", paddingLeft: "0.5rem", paddingTop: "0.3rem", backgroundColor: "rgba(0, 0, 0, 0.2)"}}>
+                                    <h4>{this.props.galleryInput[i].caption_title}</h4>
+                                    <p>{this.props.galleryInput[i].caption_subtitle}</p>
+                                </div>
+                            </div>
+                        
+                    </div>
+                );
+            }
+            
+            this.setState({
+                content: t
+            })
+
+            console.log(this.state.content)
+        }
+    }
+
+    render(){
+        console.log("Render")
+        console.log(this.state.content)
+        return (
+            <div style={font_style}>
+                <div class="row">
+                    <div class="col-md-12 d-flex justify-content-center mb-5">
+                        <button type="button" class="btn btn-outline-black waves-effect filter" data-rel="all">All</button>
+                        <button type="button" class="btn btn-outline-black waves-effect filter" data-rel="1">Mountains</button>
+                        <button type="button" class="btn btn-outline-black waves-effect filter" data-rel="2">Sea</button>
+                    </div>
+                </div>
+                
+                <div class="gallery" style={{columnCount: "3",columnWidth: "250px"}}>
+                    {this.state.content}
+                </div>
             </div>
         );
     }
-
-    return (
-        <div style={font_style}>
-            <div class="row">
-                <div class="col-md-12 d-flex justify-content-center mb-5">
-                    <button type="button" class="btn btn-outline-black waves-effect filter" data-rel="all">All</button>
-                    <button type="button" class="btn btn-outline-black waves-effect filter" data-rel="1">Mountains</button>
-                    <button type="button" class="btn btn-outline-black waves-effect filter" data-rel="2">Sea</button>
-                </div>
-            </div>
-            
-            <div class="gallery" style={{columnCount: "3",columnWidth: "250px"}}>
-                {content}
-            </div>
-        </div>
-    );
 }
+
+
+// function Gallery(props){
+//     if(!Array.isArray(props.galleryInput)){
+//         console.log("Error GalleryInput is not array")
+//         throw "Error GalleryInput is not array";
+//         return -1;
+//     }
+//     var content = [];
+
+//     let {path, url} = useRouteMatch();
+//     console.log(`${url}`)
+//     for(let i=0; i < props.galleryInput.length; i++){
+//         console.log(`${url}${props.route}/${props.galleryInput[i].id}`)
+//         content.push(
+//             <div key={i} class="mb-3" data-aos={i % 2? "fade-right" : "fade-left"}>
+                
+//                     <div style={{position: "relative", textAlign: "right", color: "white"}}>
+//                         <Link to={`${url}${props.route}/${props.galleryInput[i].id}`}>
+//                             <img class="img-fluid" src={props.galleryInput[i].img} alt="Card image cap" style={{width: "100%"}}/>
+//                         </Link>
+//                         <div style={{position: "absolute", bottom: "0rem", right: "0rem", width: "100%", paddingRight: "0.5rem", paddingLeft: "0.5rem", paddingTop: "0.3rem", backgroundColor: "rgba(0, 0, 0, 0.2)"}}>
+//                             <h4>{props.galleryInput[i].caption_title}</h4>
+//                             <p>{props.galleryInput[i].caption_subtitle}</p>
+//                         </div>
+//                     </div>
+                
+//             </div>
+//         );
+//     }
+
+//     return (
+//         <div style={font_style}>
+//             <div class="row">
+//                 <div class="col-md-12 d-flex justify-content-center mb-5">
+//                     <button type="button" class="btn btn-outline-black waves-effect filter" data-rel="all">All</button>
+//                     <button type="button" class="btn btn-outline-black waves-effect filter" data-rel="1">Mountains</button>
+//                     <button type="button" class="btn btn-outline-black waves-effect filter" data-rel="2">Sea</button>
+//                 </div>
+//             </div>
+            
+//             <div class="gallery" style={{columnCount: "3",columnWidth: "250px"}}>
+//                 {content}
+//             </div>
+//         </div>
+//     );
+// }
 
 export default Gallery;
 
--- tea_app/src/component/postList/PostList.js
+++ tea_app/src/component/postList/PostList.js
@@ -6,18 +6,22 @@ const fetchDb = require('../util/fetchDb');
 class PostList extends React.Component{
     constructor(props){
         super(props);
+        this.handleLoadMore = this.handleLoadMore.bind(this);
         this.state = {
             posts_count: 0,
+            is_has_more: true,
+            loaded_item_number: 0,
+            load_item_number_per_time: 3,
             posts: []
         }
     }
-
     componentDidMount(){
         fetchDb.fetchPostsCountAll().then(
             (response) => {
                 console.log(response);
+                console.log(response.count);
                 this.setState({
-                    posts_count: response.count
+                    posts_count: Number(response.count)
                 })
                 return response;
             }
@@ -27,36 +31,83 @@ class PostList extends React.Component{
                 return reject
             }
         )
+        // fetchDb.fetchPostList(this.state.load_item_number_per_time,this.state.loaded_item_number).then(
+        //     (response) => {
+        //         console.log(response);
+        //         var postList = response.map((item, index, array) => {
+        //             return {
+        //                 id: item.id,
+        //                 img: item.cover_img,
+        //                 caption_title: item.title,
+        //                 caption_subtitle: item.subtitle
+        //             }
+        //         })
+        //         console.log(postList);
+        //         this.setState({
+        //             loaded_item_number: this.state.loaded_item_number + this.state.load_item_number_per_time,
+        //             posts: postList
+        //         })
+        //         return response;
+        //     }
+        // ).catch(
+        //     (reject) => {
+        //         console.log("Error: ", reject)
+        //         return reject
+        //     }
+        // )
+    }
 
-        fetchDb.fetchPostList(-1,0).then(
-            (response) => {
-                console.log(response);
-                var postList = response.map((item, index, array) => {
-                    return {
-                        id: item.id,
-                        img: item.cover_img,
-                        caption_title: item.title,
-                        caption_subtitle: item.subtitle
-                    }
-                })
-                console.log(postList);
-                this.setState({
-                    posts: postList
-                })
-                return response;
-            }
-        ).catch(
-            (reject) => {
-                console.log("Error: ", reject)
-                return reject
-            }
-        )
+    handleLoadMore(page){
+        console.log("HI")
+        console.log(this.state.posts_count)
+        console.log(this.state.loaded_item_number)
+        if(this.state.loaded_item_number < 18){
+            fetchDb.fetchPostList(this.state.load_item_number_per_time,this.state.loaded_item_number).then(
+                (response) => {
+                    console.log(response);
+                    var temp = this.state.posts;
+                    response.map((item, index, array) => {
+                        console.log(this.state.posts);
+                        temp.push(
+                            {
+                                id: item.id,
+                                img: item.cover_img,
+                                caption_title: item.title,
+                                caption_subtitle: item.subtitle
+                            }
+                        );
+                        
+                        // return 
+                    })
+                    // if(this.state.loaded_item_number > 0){
+                    //     postList.push(this.state.posts);
+                    // }
+                    console.log(this.state.posts[0].id);
+                    this.setState({
+                        loaded_item_number: this.state.loaded_item_number + this.state.load_item_number_per_time,
+                        posts: temp
+                    })
+                    return response;
+                }
+            ).catch(
+                (reject) => {
+                    console.log("Error: ", reject)
+                    return reject
+                }
+            )
+        }else{
+            this.setState({
+                is_has_more: false
+            })
+        }
     }
 
     render(){
         return (
-            <div>
-                <Gallery galleryInput={this.state.posts} route=""/>
+            <div style={{height: "100vh", overflow: "auto"}}>
+                <InfiniteScroll useWindow={false} pageStart={0} hasMore={this.state.loaded_item_number < this.state.posts_count} loadMore={this.handleLoadMore}>
+                    <Gallery galleryInput={this.state.posts} route="post"/>
+                </InfiniteScroll>
             </div>
         );
     }
