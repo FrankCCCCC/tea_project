170f1abc780e1b5175a192d0a64d55d2ca390d24
FrankCCCCC <unaxultraspaceos5@gmail.com>
2020-02-25 00:06:32 +0800

    hello from root

--- server/static_server/files/static.js
+++ server/static_server/files/static.js
@@ -7,5 +7,9 @@ app.use(cors());
 app.use(helmet())
 app.use(express.static(__dirname + '/public'));
 
+app.get('/', (req, res) => {
+    res.send('Hello World')
+})
+
 const server = app.listen(5000)
 console.log("Static Server Listen on Port: 5000");
