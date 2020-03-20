fc235c44df2006a05da3a9bc625ec4bffbb36cc3
FrankCCCCC <unaxultraspaceos5@gmail.com>
2020-03-20 11:16:37 +0800

    done ItemListPage

--- tea_app/files/src/component/pages/ItemListPage.js
+++ tea_app/files/src/component/pages/ItemListPage.js
@@ -3,36 +3,97 @@ import HeroTitle from '../hero_title/HeroTitle'
 import Gallery from '../gallery/Gallery';
 // import GalleryList from '../galleryList/GalleryList';
 import {fetchItemList, fetchItemCountAll, fetchItemList_p} from '../fetch/fetchItem';
+import AsymmetricGrid from '../grid/AsymmetricGrid'
+import InfiniteScroller from '../infiniteScroller/InfiniteScroller'
 
-function ItemListPage(props) {
-    let loadRequest = fetchItemList(1,3).then(
-        (resolve) => {
-            console.log("loadRequest")
-            console.log(resolve)
-            return resolve.map(
-                (item, index, array) => {
-                    let list_item = {
-                        id: item.id,
-                        cover_img: item.cover_img,
-                        title: item.name,
-                        subtitle: item.producer_name
-                    }
-                    return list_item
-                }
-            )
+// function ItemListPage(props) {
+//     let loadRequest = fetchItemList(1,3).then(
+//         (resolve) => {
+//             console.log("loadRequest")
+//             console.log(resolve)
+//             return resolve.map(
+//                 (item, index, array) => {
+//                     let list_item = {
+//                         id: item.id,
+//                         cover_img: item.cover_img,
+//                         title: item.name,
+//                         subtitle: item.producer_name
+//                     }
+//                     return list_item
+//                 }
+//             )
+//         }
+//     ).catch(
+//         (reject) => {return reject}
+//     )
+//     return (
+//         <div>
+//             <div style={{height: "3rem"}}></div>
+//             <HeroTitle title="我們的茶" paragraph="最傳統的凍頂烏龍茶"/>
+//             <div class="container">
+//                 {/* <GalleryList countAll={fetchItemCountAll().then((response) => {return response.count})} loadrequest={fetchItemList_p} route="item"/> */}
+//             </div>
+//         </div>
+//     );
+// }
+
+class ItemListPage extends React.Component{
+    constructor(props) {
+        super(props)
+        this.state = {
+            item: [],
+            is_loaded: false, 
         }
-    ).catch(
-        (reject) => {return reject}
-    )
-    return (
-        <div>
-            <div style={{height: "3rem"}}></div>
-            <HeroTitle title="我們的茶" paragraph="最傳統的凍頂烏龍茶"/>
-            <div class="container">
-                {/* <GalleryList countAll={fetchItemCountAll().then((response) => {return response.count})} loadrequest={fetchItemList_p} route="item"/> */}
+    }
+
+    handleLoadMore(count, offset){
+        return fetchItemList(count, offset).then(
+            (resolve) => {
+                return resolve.map((item, index, array) => {
+                    return {
+                        id: parseInt(item.id, 10), 
+                        img: String(item.cover_img), 
+                        category: item.sell_type,
+                        // caption_title: item.title,
+                        // caption_subtitle: item.subtitle,
+                        title: String(item.name),
+                        subtitle: String(item.producer_name)
+                    }
+                })
+            }
+        ).then((resolve) => {
+            return resolve.map((item, index, array) => {
+                return <Gallery uuid={parseInt(index, 10)} id={parseInt(item.id, 10)} catergory={item.catergory} img={item.img} caption_title={item.caption_title} caption_subtitle={item.caption_subtitle} title={item.title} subtitle={item.subtitle} badge={item.badge} route={"item"}/>
+            })
+        })
+    }
+
+    adapter(grid_items){
+        return (
+            <AsymmetricGrid items={grid_items} column_count={3} column_width={"2rem"} column_width={"20rem"}/>
+        )
+    }
+
+    componentDidMount(){
+
+    }
+
+    componentDidUpdate(){
+
+    }
+
+    render(){
+        return (
+            <div>
+                <div style={{height: "3rem"}}></div>
+                <HeroTitle title="我們的茶" paragraph="最傳統的凍頂烏龍茶"/>
+                <div class="container">
+                    {/* <GalleryList countAll={fetchItemCountAll().then((response) => {return response.count})} loadrequest={fetchItemList_p} route="item"/> */}
+                    <InfiniteScroller count_all={fetchItemCountAll().then((response) => {return response.count})} load_request={this.handleLoadMore} grid_adapter={this.adapter} load_item_number_per_time={3}/>
+                </div>
             </div>
-        </div>
-    );
+        )
+    }
 }
 
 export default ItemListPage;
